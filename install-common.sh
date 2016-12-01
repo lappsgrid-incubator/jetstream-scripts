2750abbba14e0fb068323a7a51afe4e3f34a8711
Keith Suderman <suderman@cs.vassar.edu>
2016-12-01 14:12:08 -0500

    Fixed typo in the install-common.sh script.

--- install-common.sh
+++ install-common.sh
@@ -2,6 +2,6 @@
 
 # Commonly needed programs and utilities not included in the default images.
 
-apt-get install -y git zup unzip emacs24-nox
+apt-get install -y git zip unzip emacs24-nox
 apt-get install -y apt-transport-https ca-certificates
 apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
