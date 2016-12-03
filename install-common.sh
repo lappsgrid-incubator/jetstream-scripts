619f42356f423bd432e4f7d49be7eb40186dcb10
Keith Suderman <suderman@cs.vassar.edu>
2016-12-03 01:26:34 -0500

    Fixes install-common failing on some systems.

--- install-common.sh
+++ install-common.sh
@@ -4,4 +4,7 @@
 
 apt-get install -y git zip unzip emacs24-nox
 apt-get install -y apt-transport-https ca-certificates
-apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+
+# I am not sure what needs this, but including it causes installation to fail on 
+# some systems. Excluding it does not seem to have any negative effects.
+#apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
