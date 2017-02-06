190104f5f0e86aa13b01f9a01a3237125bc47513
Keith Suderman <suderman@cs.vassar.edu>
2017-02-06 18:32:04 -0500

    Added CentOS version of scripts.

--- install-postgres.sh
+++ install-postgres.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
-wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
+os=$(head -1 /etc/os-release | cut -f\" -f2)
+if [ "$os" = "CentOS Linux" ] ; then
+	yum install -y https://download.postgresql.org/pub/repos/yum/9.6/redhat/rhel-7-x86_64/pgdg-redhat96-9.6-3.noarch.rpm
+	yum install postgresql96-server
+elif [ "$os" = "Ubuntu" ] ; then
+	sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+	wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
 
-sudo apt-get update
-sudo apt-get install -y postgresql postgresql-contrib
+	sudo apt-get update
+	sudo apt-get install -y postgresql postgresql-contrib
+else
+	echo "Unrecognized Linux flavor."
+	exit 1
+fi
