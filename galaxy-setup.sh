3528872d0f1ddde95cbb15f3ba092a0cfd0de551
Keith Suderman <suderman@cs.vassar.edu>
2017-08-04 11:30:17 -0500

    Wait for yum.pid on CentOS

--- galaxy-setup.sh
+++ galaxy-setup.sh
@@ -5,6 +5,15 @@ export PORT=80
 
 source <(curl -sSL http://downloads.lappsgrid.org/scripts/sniff.sh)
 
+if [[ $OS = centos ]] ; then
+	# New CentOS instances on Jetstream spend a lot of time updating
+	# when they are first launched.
+	while [ -e /var/run/yum.pid ] ; do
+		echo "Waiting for a previous yum installation..."
+		sleep 10
+	done
+fi
+
 curl -sSL http://downloads.lappsgrid.org/scripts/install-common.sh | bash
 curl -sSL http://downloads.lappsgrid.org/scripts/install-java.sh | bash
 curl -sSL http://downloads.lappsgrid.org/scripts/install-postgres.sh | bash
