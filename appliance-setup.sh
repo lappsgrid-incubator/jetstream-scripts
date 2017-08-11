6a8ee54be12dd47936c7ecb694fa64cb958e0ed7
Keith Suderman <suderman@cs.vassar.edu>
2017-08-11 11:27:12 -0500

    Added appliance setup script

---
+++ appliance-setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+source <(curl -sSL http://downloads.lappsgrid.org/scripts/sniff.sh)
+
+if [[ $OS = centos ]] ; then
+	# New CentOS instances on Jetstream spend a lot of time updating
+	# when they are first launched.
+	while [ -e /var/run/yum.pid ] ; do
+		echo "Waiting for a previous yum installation..."
+		sleep 10
+	done
+fi
+
+curl -sSL http://downloads.lappsgrid.org/scripts/install-common.sh | bash
+curl -sSL http://downloads.lappsgrid.org/scripts/install-java.sh | bash
+curl -sSL http://downloads.lappsgrid.org/scripts/install-groovy.sh | bash
+curl -sSL http://downloads.lappsgrid.org/scripts/install-docker.sh | bash
+
+wget http://downloads.lappsgrid.org/ToolConfEditor-latest.tgz
+tar xzf ToolConfEditor-latest.tgz
+chmod +x tce
+mv tcd ToolConfEditor*.jar /usr/local/bin
+
+git clone -b develop https://github.com/lappsgrid-incubator/galaxy-appliance
+cd galaxy-example
+
+
