35af7d37a1d04d4522b1c7ec8ddad748305f3ca8
Keith Suderman <suderman@cs.vassar.edu>
2017-08-11 14:18:07 -0500

    Start service on CentOS

--- install-docker.sh
+++ install-docker.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 # This script seems to work on CentOS/RedHat and Ubuntu
+# 1. The docker service is not started on CentOS. Don't know about RedHat.
+# 
+
+if [ -z "$OS" ] ; then
+	source <(curl -sSL http://downloads.lappsgrid.org/scripts/sniff.sh)
+fi
 
 COMPOSE_VERSION=1.8.1
 
@@ -18,3 +24,6 @@ echo "Installing Docker Compose."
 curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/bin/docker-compose
 chmod +x /usr/bin/docker-compose
 
+if [[ $OS = centos ]] ; then
+	systemctl start docker
+fi
