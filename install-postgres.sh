342f271f76817df2c1a738e3abab0de91c160ed3
Keith Suderman <suderman@cs.vassar.edu>
2021-01-28 16:20:21 -0500

    Don't install postgres with sudo

--- install-postgres.sh
+++ install-postgres.sh
@@ -24,8 +24,8 @@ elif [[ $OS == *ubuntu* ]] ; then
 	sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 	wget -q https://www.postgresql.org/media/keys/ACCC4CF8.asc -O - | sudo apt-key add -
 
-	sudo apt-get update
-	DEBIAN_FRONTEND=noninteractive sudo apt-get install -y -q postgresql-9.6 postgresql-contrib-9.6
+	apt-get update
+	DEBIAN_FRONTEND=noninteractive apt-get install -y -q postgresql-9.6 postgresql-contrib-9.6
 else
 	echo "Unrecognized Linux flavor. Unable to install Postgres"
 	exit 1
